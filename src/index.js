97823463a2d72f899e7c76dc137e9cb5ceb4747d
Luis Zubia <soyzubia@gmail.com>
2024-03-12 02:36:29 -0700

    Include react App in index.js entry point to render a random Greeting message retrieved from the api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
+import setupStore from './redux/store';
+import { fetchRandom } from './redux/greetings/greetingsActions';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const appStore = setupStore();
+
+const rootTarget = document.getElementById('root');
+const root = createRoot(rootTarget);
+
+appStore.dispatch(fetchRandom());
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={appStore}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
 
